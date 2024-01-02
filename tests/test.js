1c764141124b5dfcc764a1dd17e0fe41ee6ca5d5
Innocent WK Chinyemba <innocentwkc@gmail.com>
2024-01-02 19:35:03 +0200

    update tests and package name

--- tests/test.js
+++ tests/test.js
@@ -2,5 +2,7 @@
 
 import { happyNewYear } from '../lib/index.js';
 
-const countdown = countdownToNewYear();
+const countdown = happyNewYear();
+
+console.log(countdown);
 console.log(`Time left until New Year: ${countdown.days} days, ${countdown.hours} hours, ${countdown.minutes} minutes, and ${countdown.seconds} seconds.`);
